196ef1342990aa253c7e8c002f550d62a72d9c92
Ilya Semibratov <semibratov@vtb.ru>
2026-08-12 16:23:50 +0300

    HW2 is completed

--- hw02_unpack_string/unpack.go
+++ hw02_unpack_string/unpack.go
@@ -2,11 +2,43 @@ package hw02unpackstring
 
 import (
 	"errors"
+	"strconv"
+	"strings"
+	"unicode"
 )
 
 var ErrInvalidString = errors.New("invalid string")
 
-func Unpack(_ string) (string, error) {
-	// Place your code here.
-	return "", nil
+func Unpack(input string) (string, error) {
+	runes := []rune(input)
+	var result strings.Builder
+
+	for i := 0; i < len(runes); i++ {
+		current := runes[i]
+		if unicode.IsDigit(current) {
+			return "", ErrInvalidString
+		}
+
+		if current == '\\' {
+			i++
+			if i == len(runes) || (runes[i] != '\\' && !unicode.IsDigit(runes[i])) {
+				return "", ErrInvalidString
+			}
+			current = runes[i]
+		}
+
+		repeatCount := 1
+		if i+1 < len(runes) && unicode.IsDigit(runes[i+1]) {
+			var err error
+			repeatCount, err = strconv.Atoi(string(runes[i+1]))
+			if err != nil {
+				return "", ErrInvalidString
+			}
+			i++
+		}
+
+		result.WriteString(strings.Repeat(string(current), repeatCount))
+	}
+
+	return result.String(), nil
 }
